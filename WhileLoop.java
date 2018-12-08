95c21c179423477203979ed3053c72fb2b3dd964
Dean Asadi <eng.de86@gmail.com>
2018-12-08 13:40:50 -0600

    While loop

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,5 +1,14 @@
 package day1.examples;
 
 public class WhileLoop {
+	public static void main (String[] arg) {
+		
+		int x = -10;
+		
+		while ( x < 0 ) {
+			System.out.println("x " + x);
+			x++;
+		}
+	}
 
 }
