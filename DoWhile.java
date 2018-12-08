0b5af74067ce9ea8e5a7c0ab40237e37996ddeef
Dean Asadi <eng.de86@gmail.com>
2018-12-08 14:12:03 -0600

    Do While loop

---
+++ DoWhile.java
@@ -0,0 +1,12 @@
+package day1.examples;
+
+public class DoWhile {
+	public static void main (String[] arg) {
+		
+		int x = 10;
+		do {
+			System.out.println("x " + x);
+			x++;
+		} while ( x < 12 );	
+	}
+}
